a50fe161faf51d756351f6c9b3ee1490af90ed18
sunnyraj10411 <sunnyraj10411@gmail.com>
2017-11-07 14:54:16 -0500

    [sunny] initial files for quantum project

---
+++ quantumCircuit.py
@@ -0,0 +1,22 @@
+import numpy as np
+import tensorflow as tf
+
+class QuantumCircuit:
+    
+    def __init__(self,  numTensor):
+        self.dimension = numTensor
+        print(self.dimension)
+
+
+def main():
+    quantumCircuit = QuantumCircuit(10)
+
+
+
+
+if __name__ == "__main__":main()
+
+
+
+
+
